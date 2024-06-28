c2b0e69b13067b3d88c41d9ee0f05ffd7513449f
abisheksde <abisheksde@gmail.com>
2024-06-28 11:27:29 +0530

    Js

---
+++ Java/Sumof2Prime.java
@@ -0,0 +1,32 @@
+public class Sumof2Prime {
+    public static void main(String[] args) {
+        
+        int num = 100;
+        int originalNumber = num;
+
+        for (int i = 2; i < num; i++) {
+            if(checkPrime(i)){
+                if(checkPrime(num-i)){
+                    System.out.printf("%d = %d + %d \n", num, i, num-i);
+                }
+            }
+        }
+
+
+        
+            
+    }
+
+    public static boolean checkPrime(int num) {
+        boolean isPrime = true;
+        for (int i = 2; i < num; i++) {
+            if(num%i==0){
+                isPrime = false;
+                return isPrime;
+            }
+        }
+        return isPrime;
+    }
+}
+
+//Shuffle Deck of Card
